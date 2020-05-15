cb86a0213ad63257b6eec7d7dc376cf9da0c14bf
Gurkirat Singh <tbhaxor@gmail.com>
2020-05-15 15:54:42 +0530

    added support http and https

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,23 +1,44 @@
 import * as https from 'https';
+import * as http from 'http';
 
 export default function (url: string): Promise<string> {
   return new Promise<string>((resolve, reject) => {
-    https
-      .get(url, (response) => {
-        response.setEncoding('utf8');
+    if (url.startsWith('https://')) {
+      https
+        .get(url, (response) => {
+          response.setEncoding('utf8');
 
-        let body = '';
+          let body = '';
 
-        response.on('data', (chunk) => {
-          body += chunk.toString();
-        });
+          response.on('data', (chunk) => {
+            body += chunk.toString();
+          });
 
-        response.on('end', () => {
-          resolve(body);
-        });
+          response.on('end', () => {
+            resolve(body);
+          });
 
-        response.on('error', reject);
-      })
-      .on('error', reject);
+          response.on('error', reject);
+        })
+        .on('error', reject);
+    } else {
+      http
+        .get(url, (response) => {
+          response.setEncoding('utf8');
+
+          let body = '';
+
+          response.on('data', (chunk) => {
+            body += chunk.toString();
+          });
+
+          response.on('end', () => {
+            resolve(body);
+          });
+
+          response.on('error', reject);
+        })
+        .on('error', reject);
+    }
   });
 }
